f81f8ea7556cf45eefadb42c0d1692e15ad04f62
Alexander <sasha.kudasov04@gmail.com>
2021-05-28 16:35:20 +0500

    Select tiles to place

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -10,6 +10,7 @@ use renderer::*;
 pub struct Game {
     renderer: Renderer,
     model: Model,
+    selected_tile: Option<Tile>,
 }
 
 impl Game {
@@ -17,6 +18,7 @@ impl Game {
         Self {
             renderer: Renderer::new(),
             model: Model::new(),
+            selected_tile: None,
         }
     }
 
@@ -25,17 +27,50 @@ impl Game {
 
         if is_mouse_button_down(MouseButton::Left) {
             let tile_pos = self.mouse_tile_pos();
-            self.model.tiles.insert(
-                tile_pos,
-                Tile {
-                    updated: false,
-                    needs_update: true,
-                    position: tile_pos,
-                    content: TileContent::Solid {
-                        tile_solid: TileSolid::Sand,
-                    },
-                },
-            );
+            match &self.selected_tile {
+                Some(tile) => {
+                    self.model.tiles.insert(
+                        tile_pos,
+                        Tile {
+                            position: tile_pos,
+                            ..tile.clone()
+                        },
+                    );
+                }
+                None => {
+                    self.model.tiles.remove(&tile_pos);
+                }
+            }
+        }
+
+        if let Some(tile_content) = if is_key_down(KeyCode::Key1) {
+            self.selected_tile = None;
+            None
+        } else if is_key_down(KeyCode::Key2) {
+            Some(TileContent::Solid {
+                tile_solid: TileSolid::Barrier,
+            })
+        } else if is_key_down(KeyCode::Key3) {
+            Some(TileContent::Solid {
+                tile_solid: TileSolid::Sand,
+            })
+        } else if is_key_down(KeyCode::Key4) {
+            Some(TileContent::Liquid {
+                tile_liquid: TileLiquid::Water,
+            })
+        } else if is_key_down(KeyCode::Key5) {
+            Some(TileContent::Gas {
+                tile_gas: TileGas::Smoke,
+            })
+        } else {
+            None
+        } {
+            self.selected_tile = Some(Tile {
+                updated: false,
+                needs_update: true,
+                position: IVec2::ZERO,
+                content: tile_content,
+            })
         }
     }
 
